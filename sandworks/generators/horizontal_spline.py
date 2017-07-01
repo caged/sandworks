fc7b24a6daa39bf4e12b1db18473e16a5dd72e2d
Caged <justin@labratrevenge.com>
2017-06-30 18:13:25 -0700

    spike out horizontal line generator

--- sandworks/generators/horizontal_spline.py
+++ sandworks/generators/horizontal_spline.py
@@ -6,11 +6,27 @@ from numpy import zeros
 from numpy import column_stack
 
 from sand import Sand
+from ..lib.color import hex_to_rgb_decimal
 
 
-class Generator:
-    def __init__(self, args):
-        count = args.count
-        sand = Sand(SIZE)
-        sand.set_bg(BG)
-        sand.set_rgba(FRONT)
+def generate(args):
+    # Number of lines
+    line_count = args.lines
+
+    # Margin as a percent of total canvas size
+    margin = 0.05
+
+    # Convert colors to RGB decimal
+    sand_color = hex_to_rgb_decimal(args.color)
+    bg_color = hex_to_rgb_decimal(args.bg_color)
+
+    # Set alpha
+    sand_color.append(0.1)
+    bg_color.append(1)
+
+    sand = Sand(args.size)
+    sand.set_rgba(sand_color)
+    sand.set_bg(bg_color)
+
+    for index, ypos in enumerate(linspace(margin, 1.0 - margin, line_count)):
+        print(index, ypos)
