6e095c6fc18b4bc31cd9742b21a43bc6e209a163
Caged <justin@labratrevenge.com>
2017-06-30 18:13:14 -0700

    no need to manage any state, so nix the OOP

--- sandworks/generators/playground.py
+++ sandworks/generators/playground.py
@@ -13,97 +13,96 @@ from sand import Sand
 from ..lib.color import hex_to_rgb_decimal
 
 
-class Generator:
-    def __init__(self, args):
-        count = args.count
+def generate(args):
+    count = args.count
 
-        # Convert colors to RGB decimal
-        sand_color = hex_to_rgb_decimal(args.color)
-        bg_color = hex_to_rgb_decimal(args.bg_color)
+    # Convert colors to RGB decimal
+    sand_color = hex_to_rgb_decimal(args.color)
+    bg_color = hex_to_rgb_decimal(args.bg_color)
 
-        # Set alpha
-        sand_color.append(0.1)
-        bg_color.append(1)
+    # Set alpha
+    sand_color.append(0.1)
+    bg_color.append(1)
 
-        distributions = [
-            'random',
-            'standard_exponential',
-            'standard_cauchy',
-            'standard_normal'
-        ]
+    distributions = [
+        'random',
+        'standard_exponential',
+        'standard_cauchy',
+        'standard_normal'
+    ]
 
-        group_a = [
-            'paint_strokes'
-        ]
+    group_a = [
+        'paint_strokes'
+    ]
 
-        group_b = [
-            'paint_filled_circles',
-            'paint_circles'
-        ]
+    group_b = [
+        'paint_filled_circles',
+        'paint_circles'
+    ]
 
-        group_c = [
-            'paint_dots'
-        ]
+    group_c = [
+        'paint_dots'
+    ]
 
-        group_d = [
-            'paint_triangles'
-        ]
+    group_d = [
+        'paint_triangles'
+    ]
 
-        for group in group_a:
-            for distribution in distributions:
-                print('Generating {} with {} distribution'.format(group, distribution))
+    for group in group_a:
+        for distribution in distributions:
+            print('Generating {} with {} distribution'.format(group, distribution))
 
-                sand = Sand(args.size)
-                sand.set_bg(bg_color)
-                sand.set_rgba(sand_color)
+            sand = Sand(args.size)
+            sand.set_bg(bg_color)
+            sand.set_rgba(sand_color)
 
-                method = getattr(numpy.random, distribution)
-                aa = method((count, 2))
-                bb = method((count, 2))
-                cc = randint(0, 500, size=count)
-                getattr(sand, group)(aa, bb, cc)
-                sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
+            method = getattr(numpy.random, distribution)
+            aa = method((count, 2))
+            bb = method((count, 2))
+            cc = randint(0, 500, size=count)
+            getattr(sand, group)(aa, bb, cc)
+            sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
 
-        for group in group_b:
-            for distribution in distributions:
-                print('Generating {} with {} distribution'.format(group, distribution))
+    for group in group_b:
+        for distribution in distributions:
+            print('Generating {} with {} distribution'.format(group, distribution))
 
-                sand = Sand(args.size)
-                sand.set_bg(bg_color)
-                sand.set_rgba(sand_color)
+            sand = Sand(args.size)
+            sand.set_bg(bg_color)
+            sand.set_rgba(sand_color)
 
-                method = getattr(numpy.random, distribution)
-                aa = method((count, 2))
-                bb = method(count)
-                cc = randint(0, 250, size=count)
-                getattr(sand, group)(aa, bb, cc)
-                sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
+            method = getattr(numpy.random, distribution)
+            aa = method((count, 2))
+            bb = method(count)
+            cc = randint(0, 250, size=count)
+            getattr(sand, group)(aa, bb, cc)
+            sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
 
-        for group in group_c:
-            for distribution in distributions:
-                print('Generating {} with {} distribution'.format(group, distribution))
+    for group in group_c:
+        for distribution in distributions:
+            print('Generating {} with {} distribution'.format(group, distribution))
 
-                sand = Sand(args.size)
-                sand.set_bg(bg_color)
-                sand.set_rgba(sand_color)
+            sand = Sand(args.size)
+            sand.set_bg(bg_color)
+            sand.set_rgba(sand_color)
 
-                method = getattr(numpy.random, distribution)
-                aa = method((count * 100, 2))
-                getattr(sand, group)(aa)
-                sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
+            method = getattr(numpy.random, distribution)
+            aa = method((count * 100, 2))
+            getattr(sand, group)(aa)
+            sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
 
-        for group in group_d:
-            for distribution in distributions:
-                print('Generating {} with {} distribution'.format(group, distribution))
+    for group in group_d:
+        for distribution in distributions:
+            print('Generating {} with {} distribution'.format(group, distribution))
 
-                sand = Sand(args.size)
-                sand.set_bg(bg_color)
-                sand.set_rgba(sand_color)
+            sand = Sand(args.size)
+            sand.set_bg(bg_color)
+            sand.set_rgba(sand_color)
 
-                method = getattr(numpy.random, distribution)
-                aa = method((count, 2))
-                bb = method((count, 2))
-                cc = method((count, 2))
-                dd = randint(0, 500, size=count)
-                getattr(sand, group)(aa, bb, cc, dd)
-                sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
+            method = getattr(numpy.random, distribution)
+            aa = method((count, 2))
+            bb = method((count, 2))
+            cc = method((count, 2))
+            dd = randint(0, 500, size=count)
+            getattr(sand, group)(aa, bb, cc, dd)
+            sand.write_to_png('tmp/{}-{}.png'.format(group, distribution))
