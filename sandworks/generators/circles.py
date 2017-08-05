7d07807c31fc0fdc28547033095819abf7ca5502
Caged <justin@labratrevenge.com>
2017-08-05 08:54:37 -0700

    support for simple circles radiating from center

--- sandworks/generators/circles.py
+++ sandworks/generators/circles.py
@@ -1,3 +1,5 @@
+from time import time
+
 from numpy import pi
 from numpy import array
 from numpy.random import random
@@ -12,7 +14,7 @@ from numpy import sin
 import cairocffi as cairo
 from sand import Sand
 from ..lib.sand_spline import SandSpline
-from ..lib.helpers import hex_to_rgb_decimal, SimpleLinearScale, get_colors
+from ..lib.helpers import hex_to_rgb_decimal, SimpleLinearScale, get_colors, _rnd_interpolate
 
 
 def guide_iterator(x, y):
@@ -51,7 +53,7 @@ def generate(args):
     bg_color = hex_to_rgb_decimal(args.bg_color)
 
     # Set alpha
-    sand_color.append(1)
+    sand_color.append(0.1)
     bg_color.append(1)
 
     sand = Sand(width, height)
@@ -59,39 +61,15 @@ def generate(args):
     sand.set_bg(bg_color)
 
     for i in range(0, 100):
-        num_points = 800
+        num_points = 1500
         angle = random() * (pi * 2) + linspace(0, (pi * 2), num_points)
-        points = column_stack((cos(angle), sin(angle))) * (0.8 * random() * 0.4)
-        path = array([[0.5, 0.5]]) + points
+        points = column_stack((cos(angle), sin(angle))) * (1.0 * random() * 0.4)
+        path = array([[0.5, 0.5]]) + _rnd_interpolate(points, 1000, ordered=True)
+
         sand.paint_dots(path)
 
-    # sand.paint_dots(random((100000, 2)))
-    sand.write_to_png("test.png")
-    # splines = []
-    # edge = 0.08
-    # grid = 15
-    # leap_y = (1.0 - 2 * edge) / (grid - 1) * 0.5 * 0.75
-    # step = 0.0000001
-    # inum = 2000
-    # twopi = pi * 2.0
-    #
-    # splines = []
-    # for x in linspace(edge, 1.0-edge, grid):
-    #     for y in linspace(edge, 1.0-edge, grid):
-    #         guide = guide_iterator(x, y)
-    #         pnum = randint(10, 150)
-    #
-    #         a = random() * twopi + linspace(0, twopi, pnum)
-    #         path = column_stack((cos(a), sin(a))) * leap_y
-    #
-    #         scale = arange(pnum).astype('float') * step
-    #
-    #         s = SandSpline(guide, path, inum, scale)
-    #     splines.append(s)
-    #
-    # colors = get_colors('p1.png')
-    # nc = len(colors)
-    #
-    # while True:
-    #     for s in splines:
-    #         print(s)
+    file_name = '{}/{}.png'.format(
+        args.out_dir,
+        int(time()))
+
+    sand.write_to_png(file_name)
