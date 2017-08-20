ab75c825252a110c862943354b5503a2d6df15fd
Caged <justin@labratrevenge.com>
2017-08-20 10:47:25 -0700

    try this import

--- sandworks/__init__.py
+++ sandworks/__init__.py
@@ -6,5 +6,6 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
-# from sandworks.lib import helpers  # nopep8
-# from sandworks.lib import sand_spline  # nopep8
+print(sys.path)
+from lib import helpers  # nopep8
+from lib import sand_spline  # nopep8
