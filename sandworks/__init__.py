09aa9da349c17d0077537fe5a53e9da24686cfa4
Caged <justin@labratrevenge.com>
2017-08-20 10:41:31 -0700

    do we really need this?

--- sandworks/__init__.py
+++ sandworks/__init__.py
@@ -6,5 +6,5 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
-from sandworks.lib import helpers  # nopep8
-from sandworks.lib import sand_spline  # nopep8
+# from sandworks.lib import helpers  # nopep8
+# from sandworks.lib import sand_spline  # nopep8
